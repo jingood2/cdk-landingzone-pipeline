3cc6094093c2204decc83f1cde58a6e2d56ac80e
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-08-08 12:46:50 +0900

    update lggoing bucket

--- src/lib/my-template-stack.ts
+++ src/lib/my-template-stack.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cfn_inc from '@aws-cdk/cloudformation-include';
 import * as cdk from '@aws-cdk/core';
 
@@ -12,12 +13,17 @@ export class MyTemplateStack extends cdk.Stack {
     super(scope, id, props);
 
     // add cfn
-    new cfn_inc.CfnInclude(this, 'logging-template', {
+    const cfnTemplate = new cfn_inc.CfnInclude(this, 'logging-template', {
       templateFile: path.join(__dirname, '..', 'cfn-template/master/01.audit/logging.template.yaml'),
       /* parameters: {
         ['BucketName']: 'jingood0604-mutation-bucket',
       }, */
     });
+
+    const stack = cdk.Stack.of(this);
+
+    const cfnBucket = cfnTemplate.getResource('LoggingBucket') as s3.CfnBucket;
+    cfnBucket.bucketName = `${stack.stackName}-logging-${stack.account}`;
 
   }
 }
