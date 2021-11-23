a40467db903f527bc13496dfd99567fe6043f9af
jingood2 <jingood2@gmail.com>
2021-11-23 22:03:32 +0900

    update

--- src/lib/core-accounts/iam-group-construct.ts
+++ src/lib/core-accounts/iam-group-construct.ts
@@ -181,7 +181,7 @@ export class IamGroupConstruct extends cdk.Construct {
       'ce:GetCostAndUsage',
     );
     p2.addAllResources();
-    p2.addConditions({ boolIfExists: { 'aws:MultiFactorAuthPresent': 'false' } });
+    p2.addCondition( 'boolIfExists', { 'aws:MultiFactorAuthPresent': 'false' } );
     customPolicyDocument.addStatements(p2);
     /*
 
