f8817eb2c25c2772a6858e0b0b3614535d8cd52f
jingood2 <jingood2@gmail.com>
2021-11-23 23:36:27 +0900

    update

--- src/lib/core-accounts/iam-group-construct.ts
+++ src/lib/core-accounts/iam-group-construct.ts
@@ -152,9 +152,27 @@ export class IamGroupConstruct extends cdk.Construct {
 
     const customPolicyDocument = iam.PolicyDocument.fromJson(policyDocument);
 
+    const p2 = new iam.PolicyStatement();
+    p2.sid = 'BlockMostAccessUnlessSignedInWithMFA';
+    p2.effect = iam.Effect.DENY;
+    p2.addNotActions(
+      'iam:CreateVirtualMFADevice',
+      'iam:EnableMFADevice',
+      'iam:ListMFADevices',
+      'iam:ListUsers',
+      'iam:ListVirtualMFADevices',
+      'iam:ResyncMFADevice',
+      'sts:AssumeRole',
+      'iam:ListAccountAliases',
+      'ce:GetCostAndUsage',
+    );
+    p2.addAllResources();
+    p2.addCondition( 'BoolIfExists', { 'aws:MultiFactorAuthPresent': 'false' } );
+    customPolicyDocument.addStatements(p2);
+
     if ( envVars.MASTER.REQUIRE_MFA_ON_MAIN_ACCOUNT_ACTION == 'true' ) {
 
-      const p2 = new iam.PolicyStatement();
+    /*   const p2 = new iam.PolicyStatement();
       p2.sid = 'BlockMostAccessUnlessSignedInWithMFA';
       p2.effect = iam.Effect.DENY;
       p2.addNotActions(
@@ -181,7 +199,7 @@ export class IamGroupConstruct extends cdk.Construct {
       //p3.addConditions({ BoolIfExists: { 'aws:MultiFactorAuthPresent': 'false' } });
 
       customPolicyDocument.addStatements(p2);
-      customPolicyDocument.addStatements(p3);
+      customPolicyDocument.addStatements(p3); */
     };
 
     const userCredentialsManagementPolicy = new iam.Policy(this, 'UserCredentialsManagementPolicy', {
