afc38edfdabca3f780ef8bdef033fbb9bb705ae5
jingood2 <jingood2@gmail.com>
2021-11-24 14:38:36 +0900

    update

--- src/lib/stac-sets/01-password-policy.ts
+++ src/lib/stac-sets/01-password-policy.ts
@@ -11,7 +11,7 @@ export class PasswordPolicy extends cdk.Construct {
 
     new cdk.CfnStackSet(this, 'password-policy', {
       stackSetName: 'the-new-password-policy',
-      permissionModel: 'SELF_MANAGED',
+      permissionModel: 'SERVICE_MANAGED',
       capabilities: ['CAPABILITY_IAM'],
       stackInstancesGroup: [
         {
