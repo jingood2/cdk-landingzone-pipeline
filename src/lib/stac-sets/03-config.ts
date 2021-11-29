de7a85ba7f7ee06634338dfd6f06562cf1b9b542
jingood2 <jingood2@gmail.com>
2021-11-30 00:53:08 +0900

    update parameter

--- src/lib/stac-sets/03-config.ts
+++ src/lib/stac-sets/03-config.ts
@@ -15,28 +15,28 @@ export class StacksetConfig extends cdk.Construct {
       permissionModel: 'SELF_MANAGED',
       capabilities: ['CAPABILITY_NAMED_IAM'],
       administrationRoleArn: `arn:aws:iam::${envVars.MASTER.ACCOUNT_ID}:role/AWSCloudFormationStackSetAdministrationRole`,
-      parameters: [{
-        parameterKey: 'LoggingAccount',
-        parameterValue: `${envVars.LOG_ARCHIVE.ACCOUNT_ID}`,
-      },
-      {
-        parameterKey: 'DeliveryFrequency',
-        parameterValue: 'One_Hour',
-      },
-      {
-        parameterKey: 'ConfigBucketPrefix',
-        parameterValue: 'new-audit-storage-config',
-      },
-      {
-        parameterKey: 'GlobalConfigRegion',
-        parameterValue: 'ap-northeast-2',
-      }],
       stackInstancesGroup: [
         {
           regions: ['ap-northeast-2'],
           deploymentTargets: {
             accounts: envVars.SERVICE_ACCOUNTS.map(value => { return value.Id; }),
           },
+          parameterOverrides: [{
+            parameterKey: 'LoggingAccount',
+            parameterValue: `${envVars.LOG_ARCHIVE.ACCOUNT_ID}`,
+          },
+          {
+            parameterKey: 'DeliveryFrequency',
+            parameterValue: 'One_Hour',
+          },
+          {
+            parameterKey: 'ConfigBucketPrefix',
+            parameterValue: 'new-audit-storage-config',
+          },
+          {
+            parameterKey: 'GlobalConfigRegion',
+            parameterValue: 'ap-northeast-2',
+          }],
         },
       ],
       templateBody: convertYamlString(path.join(__dirname, '../..', 'cfn-template/stack-set/03.auditing-configuration/config.template.yaml')),
