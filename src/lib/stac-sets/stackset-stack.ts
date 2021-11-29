429cf5fbe95005f7efcd2e0ef127b77074eae5e9
jingood2 <jingood2@gmail.com>
2021-11-29 23:56:09 +0900

    Add StackSet Config

--- src/lib/stac-sets/stackset-stack.ts
+++ src/lib/stac-sets/stackset-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import { PasswordPolicy } from './02-password-policy';
+import { StacksetConfig } from './03-config';
 //import { PasswordPolicy } from '../stac-sets/02-password-policy';
 
 export interface StacksetStackProps extends cdk.StackProps {
@@ -11,6 +12,8 @@ export class StacksetStack extends cdk.Stack {
     super(scope, id, props);
 
     new PasswordPolicy(this, 'password-policy');
+
+    new StacksetConfig(this, 'stackset-config');
 
 
   }
